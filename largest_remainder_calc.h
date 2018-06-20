147da2f275251fa9d18f3302001fd4c66df99314
Sam Cantrell <samuelcantrell@gmail.com>
2018-06-20 16:42:16 -0700

    Cleaning up comments and line lengths.

--- largest_remainder_calc.h
+++ largest_remainder_calc.h
@@ -8,8 +8,8 @@ in making our determination */
 #define INTERNAL_FALSE	0
 #define INTERNAL_TRUE	!(INTERNAL_FALSE)
 
-long largest_remainder_calc(long num_seats,
-			long num_parties,
+long largest_remainder_calc(long n_seats,
+			long n_parties,
 			long votes[],
 			int quota_method,
 			long results[]);
@@ -17,56 +17,62 @@ long rank(float numbers[], long len, float n);
 void sort_array_desc(float numbers[], long len);
 int float_equals(float a, float b);
 
-long largest_remainder_calc(long num_seats, long num_parties, long votes[], int quota_method, long results[]) {
-	/* calc the total number of votes */
-	long total_votes = 0;
-	for (int i = 0; i < num_parties; i++) {
-		total_votes += votes[i];
+long largest_remainder_calc(long n_seats,
+			long n_parties,
+			long votes[],
+			int quota_method,
+			long results[]) {
+	/* Calc the total number of votes */
+	long n_votes = 0;
+	for (int i = 0; i < n_parties; i++) {
+		n_votes += votes[i];
 	}
 
-	/* calculate the appropriate quota */
+	/* Calculate the appropriate quota */
 	float quota = 0.0;
 	if (quota_method == HARE_QUOTA) {
-		quota = total_votes / num_seats;
+		quota = n_votes / n_seats;
 	} else {
-		/* calc the DROOP quota. Note that this could also be called if an unspecified
-		value is passed for quota_method. */
-		quota = (int)(1 + (total_votes / (1 + num_seats)));
+		/* calc the DROOP quota. Note that this could also be called if
+		an unspecified value is passed for quota_method. */
+		quota = (int)(1 + (n_votes / (1 + n_seats)));
 	}
 
-	/* calculate the votes per quota for each of the parties and allocate the integer portion
-	to each of the parties. These are their automatic seats.  */
-	float votes_per_quota[num_parties];
-	long num_seats_allocated = 0;
-	for (int i = 0; i < num_parties; i++) {
+	/* Calculate the votes per quota for each of the parties and allocate
+	the integer portion to each of the parties. These are their automatic
+	seats. */
+	float votes_per_quota[n_parties];
+	long n_seats_alloc = 0;
+	for (int i = 0; i < n_parties; i++) {
 		votes_per_quota[i] = votes[i] / quota;
 
 		results[i] = (int) votes_per_quota[i];
-		num_seats_allocated += results[i];
+		n_seats_alloc += results[i];
 	}
 
-	/* check to see if we've automatically allocated all the seats, because if we have, our
-	work here is done. */
-	long difference = 0;
-	if ((difference = num_seats - num_seats_allocated) > 0) {
-		/* we haven't automatically allocated all the seats, so now we'll sort by the
-		largest remainders and give seats in that order until all seats are allocated. */
-		float remainders[num_parties];
-		for (int i = 0; i < num_parties; i++) {
+	/* Check to see if we've automatically allocated all the seats, because
+	if we have, our work here is done. */
+	long diff = 0;
+	if ((diff = n_seats - n_seats_alloc) > 0) {
+		/* We haven't automatically allocated all the seats, so now
+		we'll sort by the largest remainders and give seats in that
+		order until all seats are allocated. */
+		float remainders[n_parties];
+		for (int i = 0; i < n_parties; i++) {
 			remainders[i] = votes_per_quota[i] - (int)votes_per_quota[i];
 		}
 
-		for (int i = 1; i <= difference; i++) {
-			for (int j = 0; j < num_parties; j++) {
-				if (rank(remainders, num_parties, remainders[j]) == i) {
+		for (int i = 1; i <= diff; i++) {
+			for (int j = 0; j < n_parties; j++) {
+				if (rank(remainders, n_parties, remainders[j]) == i) {
 					results[j]++;
-					num_seats_allocated++;
+					n_seats_alloc++;
 				}
 			}
 		}
 	}
 
-	return num_seats_allocated;
+	return n_seats_alloc;
 }
 
 long rank(float numbers[], long len, float n) {
